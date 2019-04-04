a249fb2b47064cb70c680d2971503f01a6acd4de
Bernard Degive <BernardDegive@hotmail.com>
2019-04-04 13:42:11 +0200

    forecast information updated with Temp Min Max

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,13 +4,14 @@ const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/574628846c59991492dce6c65d7c81a1/' + latitude + ',' + longitude
+    console.log(url)
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + 'It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + 'Temperature min: ' + body.daily.data[0].temperatureLow + ' - temperature max: ' + body.daily.data[0].temperatureHigh + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 
